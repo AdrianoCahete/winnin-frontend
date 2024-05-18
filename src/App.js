5bdce4cb95f490b5cd04888d0f28214b2e6f7d38
Adriano Cahete <me@adrianocahete.dev>
2024-05-17 22:10:37 -0300

    feat(app): add base component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import AnimeList from "./components/AnimeList";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>AnimeList</h1>
+      <AnimeList />
     </div>
   );
 }
-
 export default App;
